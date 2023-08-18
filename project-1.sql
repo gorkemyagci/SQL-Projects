c2d68d2fd2489c2561ada1e2d0ba87a0c5669c82
Görkem Yağcı <92743361+gorkemyagci@users.noreply.github.com>
2023-08-18 19:03:52 +0300

    Update project-1.sql

--- project-1.sql
+++ project-1.sql
@@ -1,23 +1,21 @@
 -- PROJECT 1 - SQL
 
 --1
-/*SELECT title, description FROM film;*/
+SELECT title, description FROM film;*/
 --2
-/*
 SELECT * FROM film
-WHERE length > 60 AND length < 75
-*/
+WHERE length > 60 AND length < 75;
 --3
-/*
+
 SELECT * FROM film
-WHERE rental_rate = 0.99 AND replacement_cost = 12.99 OR replacement_cost = 28.99
-*/
+WHERE rental_rate = 0.99 AND replacement_cost = 12.99 OR replacement_cost = 28.99;
+
 --4
-/*SELECT * FROM customer
+SELECT * FROM customer
 WHERE first_name = 'Mary'
-last_name = 'Smith'
-*/
+last_name = 'Smith';
+
 --5
-/*SELECT * FROM film
+SELECT * FROM film
 WHERE length < 50 AND rental_rate != 2.99 OR rental_rate != 4.99
-*/
+
